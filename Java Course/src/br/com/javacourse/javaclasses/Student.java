62b5a349634c729dd22dd1d707ad18586edcb49f
Daniele Oliveira <daniele.oliveira@wingsit.com.br>
2022-11-07 18:46:53 -0300

    working with classes and objects

--- Java Course/src/br/com/javacourse/javaclasses/Student.java
+++ Java Course/src/br/com/javacourse/javaclasses/Student.java
@@ -5,5 +5,11 @@ public class Student {
     public String enrollment;
     public int age;
 
+    public void printStudentData(){
+        System.out.println(this.name);
+        System.out.println(this.enrollment);
+        System.out.println(this.age);
+    }
+
 
 }
--- Java Course/src/br/com/javacourse/javaclassestest/StudentTest.java
+++ Java Course/src/br/com/javacourse/javaclassestest/StudentTest.java
@@ -9,7 +9,14 @@ public class StudentTest {
         student1.enrollment = "123456";
         student1.age = 18;
 
+        Student student2 = new Student();
+        student2.name = "Marie";
+        student2.enrollment = "1234567";
+        student2.age = 21;
+
         System.out.println(student1.name);
+        student1.printStudentData();
+        student2.printStudentData();
 
     }
 }
