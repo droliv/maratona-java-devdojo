8c692bb8df5461984a4b11688b4ef08cccd0bfcf
Daniele Oliveira <daniele.oliveira@wingsit.com.br>
2022-11-07 13:42:44 -0300

    printing variables

--- Java Course/src/Main.java
+++ Java Course/src/Main.java
@@ -1,4 +1,4 @@
-/** Um Hello World pra não perder o hábito */
+//Um Hello World pra não perder o hábito
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ Java Course/src/PrintingVariables.java
@@ -0,0 +1,23 @@
+public class PrintingVariables {
+    public static void main(String[] args){
+        System.out.println("Imprimindo váriáveis");
+        System.out.println("Primitive Types: int, double, float, boolean, char, byte, short, long");
+        int age = 20;
+        System.out.println("int variable age value: " + age);
+        double price = 25.00;
+        System.out.println("double variable price value: " + price);
+        float priceFloat = 25000;
+        System.out.println("float variable priceFloat value: " + priceFloat);
+        boolean trueValue = true;
+        System.out.println("boolean variable trueValue value: " + trueValue);
+        char character = 'a';
+        System.out.println("char variable character value: " + character);
+        byte ageByte = 36;
+        System.out.println("byte variable age value: " + ageByte);
+        short ageShort = 4321;
+        System.out.println("short variable ageShort value: " + ageShort);
+        long longNumber = 1000L;
+        System.out.println("long variable longNumber value: " + longNumber);
+
+    }
+}
